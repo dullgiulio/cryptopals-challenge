be8141257268cfea33c49f84d3c1693732117f7b
Giulio Iotti <dullgiulio@gmail.com>
2017-10-05 12:57:01 +0000

    padding

---
+++ set2/09-padding/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func pad(bs []byte, fill byte, sz int) []byte {
+	var end int
+	for ; end < len(bs); end += sz {
+	}
+	dst := make([]byte, end, end)
+	copy(dst, bs)
+	for i := len(bs); i < end; i++ {
+		dst[i] = fill
+	}
+	return dst
+}
+
+func main() {
+	fmt.Printf("%+v\n", pad([]byte("YELLOW SUBMARINE"), byte(4), 20))
+}
