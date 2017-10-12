d682e816e020d5b11df6c9009dc7f4d4a5e7de60
Giulio Iotti <dullgiulio@gmail.com>
2017-10-12 14:03:09 +0000

    simplified sha1 implementation

---
+++ set4/28-sha1-mac/main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+)
+
+const (
+	h0 = 0x67452301
+	h1 = 0xEFCDAB89
+	h2 = 0x98BADCFE
+	h3 = 0x10325476
+	h4 = 0xC3D2E1F0
+
+	_K0 = 0x5A827999
+	_K1 = 0x6ED9EBA1
+	_K2 = 0x8F1BBCDC
+	_K3 = 0xCA62C1D6
+
+	chunk = 64
+)
+
+func sha1block(p []byte, h0, h1, h2, h3, h4 uint32) (uint32, uint32, uint32, uint32, uint32) {
+	var w [16]uint32
+
+	for len(p) >= chunk {
+		// Can interlace the computation of w with the
+		// rounds below if needed for speed.
+		for i := 0; i < 16; i++ {
+			j := i * 4
+			w[i] = uint32(p[j])<<24 | uint32(p[j+1])<<16 | uint32(p[j+2])<<8 | uint32(p[j+3])
+		}
+
+		a, b, c, d, e := h0, h1, h2, h3, h4
+
+		// Each of the four 20-iteration rounds
+		// differs only in the computation of f and
+		// the choice of K (_K0, _K1, etc).
+		i := 0
+		for ; i < 16; i++ {
+			f := b&c | (^b)&d
+			a5 := a<<5 | a>>(32-5)
+			b30 := b<<30 | b>>(32-30)
+			t := a5 + f + e + w[i&0xf] + _K0
+			a, b, c, d, e = t, a, b30, c, d
+		}
+		for ; i < 20; i++ {
+			tmp := w[(i-3)&0xf] ^ w[(i-8)&0xf] ^ w[(i-14)&0xf] ^ w[(i)&0xf]
+			w[i&0xf] = tmp<<1 | tmp>>(32-1)
+
+			f := b&c | (^b)&d
+			a5 := a<<5 | a>>(32-5)
+			b30 := b<<30 | b>>(32-30)
+			t := a5 + f + e + w[i&0xf] + _K0
+			a, b, c, d, e = t, a, b30, c, d
+		}
+		for ; i < 40; i++ {
+			tmp := w[(i-3)&0xf] ^ w[(i-8)&0xf] ^ w[(i-14)&0xf] ^ w[(i)&0xf]
+			w[i&0xf] = tmp<<1 | tmp>>(32-1)
+			f := b ^ c ^ d
+			a5 := a<<5 | a>>(32-5)
+			b30 := b<<30 | b>>(32-30)
+			t := a5 + f + e + w[i&0xf] + _K1
+			a, b, c, d, e = t, a, b30, c, d
+		}
+		for ; i < 60; i++ {
+			tmp := w[(i-3)&0xf] ^ w[(i-8)&0xf] ^ w[(i-14)&0xf] ^ w[(i)&0xf]
+			w[i&0xf] = tmp<<1 | tmp>>(32-1)
+			f := ((b | c) & d) | (b & c)
+
+			a5 := a<<5 | a>>(32-5)
+			b30 := b<<30 | b>>(32-30)
+			t := a5 + f + e + w[i&0xf] + _K2
+			a, b, c, d, e = t, a, b30, c, d
+		}
+		for ; i < 80; i++ {
+			tmp := w[(i-3)&0xf] ^ w[(i-8)&0xf] ^ w[(i-14)&0xf] ^ w[(i)&0xf]
+			w[i&0xf] = tmp<<1 | tmp>>(32-1)
+			f := b ^ c ^ d
+			a5 := a<<5 | a>>(32-5)
+			b30 := b<<30 | b>>(32-30)
+			t := a5 + f + e + w[i&0xf] + _K3
+			a, b, c, d, e = t, a, b30, c, d
+		}
+
+		h0 += a
+		h1 += b
+		h2 += c
+		h3 += d
+		h4 += e
+
+		p = p[chunk:]
+	}
+
+	return h0, h1, h2, h3, h4
+}
+
+func sha1sum(bs []byte) []byte {
+	ml := len(bs)
+	tmplen := 0
+	if ml%64 < 56 {
+		tmplen = 56 - ml%64
+	} else {
+		tmplen = 64 + 56 - ml%64
+	}
+	tmp := make([]byte, tmplen+8)
+	tmp[0] = 0x80
+	// Length in bits
+	ln := uint64(ml * 8)
+	off := uint(len(tmp) - 8)
+	for i := uint(0); i < 8; i++ {
+		tmp[off+i] = byte(ln >> (56 - 8*i))
+	}
+	bs = append(bs, tmp...)
+	h := [5]uint32{h0, h1, h2, h3, h4}
+	h[0], h[1], h[2], h[3], h[4] = sha1block(bs, h[0], h[1], h[2], h[3], h[4])
+	digest := make([]byte, 20)
+	for i, s := range h {
+		digest[i*4] = byte(s >> 24)
+		digest[i*4+1] = byte(s >> 16)
+		digest[i*4+2] = byte(s >> 8)
+		digest[i*4+3] = byte(s)
+	}
+	return digest
+}
+
+func main() {
+	hash := sha1sum([]byte(""))
+	fmt.Printf("%s\n", hex.EncodeToString(hash))
+	hash = sha1sum([]byte("The quick brown fox jumps over the lazy dog"))
+	fmt.Printf("%s\n", hex.EncodeToString(hash))
+}
